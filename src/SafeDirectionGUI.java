ac9e57ad3aca1861d12ea8fc47b0c682367aa08b
PnDong <joseph1999@naver.com>
2019-08-10 17:39:55 +0900

    modify GUI

--- src/SafeDirectionGUI.java
+++ src/SafeDirectionGUI.java
@@ -8,35 +8,39 @@ public class SafeDirectionGUI extends JFrame {
 
 	public SafeDirectionGUI() {
 		
-		setTitle("Safe Direction");
+		setTitle("안전길찾기");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//setLocationRelativeTo(null);
+		setLocation(300,30);
 		
-		Container pane = getContentPane();
-		pane.setLayout(new GridLayout(1,2));
+		Container topPane = getContentPane();
 		
-		ImageIcon mapImage = new ImageIcon("images/map_image.jpg");
-		JLabel mapLabel = new JLabel(mapImage);
-		pane.add(mapLabel);
+		topPane.add(new JPanel(),BorderLayout.WEST);
+		topPane.add(new JPanel(),BorderLayout.NORTH);
 		
-		JPanel controlPane = new JPanel();
-		Border border = controlPane.getBorder();
-		Border margin = new EmptyBorder(30,30,30,30);
-		controlPane.setBorder(new CompoundBorder(border, margin));
+		ImageIcon mapImageIcon = new ImageIcon("images/map_image.jpg");
+		Image scaledImage = mapImageIcon.getImage().getScaledInstance(mapImageIcon.getIconWidth()*3/4,mapImageIcon.getIconHeight()*3/4,Image.SCALE_DEFAULT);
+		ImageIcon scaledImageIcon = new ImageIcon(scaledImage);
+		JLabel mapLabel = new JLabel(scaledImageIcon);
+		mapLabel.setBorder(new EmptyBorder(50,50,50,50));
+		topPane.add(mapLabel,BorderLayout.CENTER);
 		
-		GridLayout controlLayout = new GridLayout(7,1);
-		controlLayout.setVgap(5);
-		controlPane.setLayout(controlLayout);
+		JPanel searchPane = new JPanel();
+		searchPane.setBorder(new EmptyBorder(200,0,50,50));
 		
-		JTextField startName = new JTextField();
-		JTextField endName = new JTextField();
-		JButton findPathBtn = new JButton("길찾기");
-		JButton setEdgeBtn = new JButton("평판");
-		controlPane.add(startName);
-		controlPane.add(endName);
-		controlPane.add(findPathBtn);
-		controlPane.add(setEdgeBtn);
-		pane.add(controlPane);
+		GridLayout searchLayout = new GridLayout(10,1);
+		searchLayout.setVgap(5);
+		searchPane.setLayout(searchLayout);
+		
+		JTextField startNameInput = new JTextField(20);
+		JTextField endNameInput = new JTextField(20);
+		JButton findPathBtn = new JButton("길 찾기");
+		searchPane.add(startNameInput);
+		searchPane.add(endNameInput);
+		searchPane.add(findPathBtn);
+		topPane.add(searchPane,BorderLayout.EAST);
+		
+		JButton estEdgeBtn = new JButton("길 평가");
+		topPane.add(estEdgeBtn,BorderLayout.SOUTH);
 		
 		pack();
 		setVisible(true);
