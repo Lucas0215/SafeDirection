1fd009f0850c66f37bc3fe548e65f78d92a6a294
PnDong <joseph1999@naver.com>
2019-08-10 16:30:35 +0900

    modify GUI code

--- src/SafeDirectionGUI.java
+++ src/SafeDirectionGUI.java
@@ -1,5 +1,8 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 
 public class SafeDirectionGUI extends JFrame {
 
@@ -10,11 +13,30 @@ public class SafeDirectionGUI extends JFrame {
 		//setLocationRelativeTo(null);
 		
 		Container pane = getContentPane();
-		pane.setLayout(new FlowLayout());
+		pane.setLayout(new GridLayout(1,2));
 		
 		ImageIcon mapImage = new ImageIcon("images/map_image.jpg");
 		JLabel mapLabel = new JLabel(mapImage);
 		pane.add(mapLabel);
+		
+		JPanel controlPane = new JPanel();
+		Border border = controlPane.getBorder();
+		Border margin = new EmptyBorder(30,30,30,30);
+		controlPane.setBorder(new CompoundBorder(border, margin));
+		
+		GridLayout controlLayout = new GridLayout(7,1);
+		controlLayout.setVgap(5);
+		controlPane.setLayout(controlLayout);
+		
+		JTextField startName = new JTextField();
+		JTextField endName = new JTextField();
+		JButton findPathBtn = new JButton("길찾기");
+		JButton setEdgeBtn = new JButton("평판");
+		controlPane.add(startName);
+		controlPane.add(endName);
+		controlPane.add(findPathBtn);
+		controlPane.add(setEdgeBtn);
+		pane.add(controlPane);
 		
 		pack();
 		setVisible(true);
